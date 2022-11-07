7523b68d568ed13bf5a9fc38248d3ac75e142ea0
ErosEros <eros.vittori1994@gmail.com>
2022-11-07 02:16:29 +0100

    TABLE

--- database/migrations/2022_11_07_004926_create_trains_table.php
+++ database/migrations/2022_11_07_004926_create_trains_table.php
@@ -22,7 +22,7 @@ class CreateTrainsTable extends Migration
     // Codice Treno
     // Numero Carrozze
     // In orario
-    // Cancellato
+    // Cancellat
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
